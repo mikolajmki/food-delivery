40feb00c88e023978e8fda9349c90152f47a5062
Mikołaj Marcinkowski <mikol4m@gmail.com>
2024-04-23 17:26:07 +0200

    cleanup

--- sql.sql
+++
@@ -1,3 +0,0 @@
-﻿SELECT * FROM [dbo].[AspNetUsers]
---update [dbo].[AspNetUsers] set LockoutEnabled = 0 where Email = 'mikol4m@gmail.com'
---update [dbo].[AspNetUsers] set EmailConfirmed = 1 where Email = 'mikol4m@gmail.com'
